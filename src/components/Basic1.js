b4235344fa65b7096b9cd680f78ea81fea12d002
s-ko <song.1209@icloud.com>
2024-01-23 23:00:01 +0900

    add: test event handler

--- src/components/Basic1.js
+++ src/components/Basic1.js
@@ -1,10 +1,15 @@
 import React from 'react'
 
 const Basic1 = (props) => {
+  const clickHandler = () => {
+    console.log('Button clicked')
+  }
+
   return (
-    <div>
+    <>
+      <button onClick={clickHandler}>Click</button>
       <h1>Hello { props.name }</h1>
-    </div>
+    </>
   )
 }
 
