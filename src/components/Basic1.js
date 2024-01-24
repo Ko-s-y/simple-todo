844721cea2cea714a53b4bc7c85a025a73765aad
s-ko <song.1209@icloud.com>
2024-01-24 22:48:57 +0900

    add: test useState

--- src/components/Basic1.js
+++ src/components/Basic1.js
@@ -1,13 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Basic1 = (props) => {
   const clickHandler = () => {
     console.log('Button clicked')
   }
 
+  const [count, setCount] = useState(0)
+
   return (
     <>
-      <button onClick={clickHandler}>Click</button>
+      {/* <button onClick={() => setCount(count+1)}>Click {count}</button> */}
+      <button onClick={() => {setCount(prevCount=>prevCount+1); setCount(prevCount=>prevCount+1);} }>Click {count}</button>
       <h1>Hello { props.name }</h1>
     </>
   )
