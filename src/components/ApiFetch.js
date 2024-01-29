a0ed22299c4a80aa767949ffad5da55410246f83
s-ko <song.1209@icloud.com>
2024-01-29 22:47:26 +0900

    add: ApiFetch.js json placeholderからfetchを用いてposts100件を取得

--- src/components/ApiFetch.js
+++ src/components/ApiFetch.js
@@ -4,9 +4,15 @@ import axios from 'axios'
 const ApiFetch = () => {
   const [posts, setPosts] = useState([])
   useEffect(() => {
-    axios.get('https://jsonplaceholder.typicode.com/posts').then(result => {
-      setPosts(result.data)
+    fetch('https://jsonplaceholder.typicode.com/posts', {method: 'GET'}).then(result => {
+      result.json().then(data => {
+        setPosts(data)
+      })
     })
+
+    // axios.get('https://jsonplaceholder.typicode.com/posts').then(result => {
+    //   setPosts(result.data)
+    // })
   }, [])
 
   return (
