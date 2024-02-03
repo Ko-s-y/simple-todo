2bdf238c558458b997023da0df69a33658d653aa
s-ko <song.1209@icloud.com>
2024-02-04 00:00:37 +0900

    add: counter for reducer

--- src/App.js
+++ src/App.js
@@ -1,17 +1,36 @@
+import React, {useReducer} from 'react'
 import logo from './logo.svg';
 import './App.css';
 import AppContext from './contexts/AppContext';
+import CompB from './components/CompB';
+// import CompB from './components/CompB';
 // import B from './components/B';
-import BasicReducer from './components/BasicReducer';
+// import BasicReducer from './components/BasicReducer';
 // import ApiFetch from './components/ApiFetch';
 // import Basic1 from './components/Basic1';
 // import Basic2 from './components/Basic2';
 // import BasicUseEffect from './components/BasicUseEffect';
 // import TimerContainer from './components/TimerContainer';
 
+const initialState = 0
+const reducer = (currentState, action) => {
+  switch(action) {
+    case 'add_1':
+      return currentState += 1;
+    case 'multiple':
+      return currentState * 3;
+    case 'reset':
+      return initialState;
+    default:
+      return currentState;
+  }
+}
+
 function App() {
+  const [count, dispatch] = useReducer(reducer, initialState)
+
   return (
-    <AppContext.Provider value={'value from App.js'}>
+    <AppContext.Provider value={{countProvided: count, dispatchProvided: dispatch}}>
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
@@ -21,7 +40,9 @@ function App() {
           {/* <TimerContainer /> */}
           {/* <ApiFetch /> */}
           {/* <B /> */}
-          <BasicReducer />
+          {/* <BasicReducer /> */}
+          Count {count}
+          <CompB />
         </header>
       </div>
     </AppContext.Provider>
---
+++ src/components/CompC.js
@@ -0,0 +1,16 @@
+import React, {useContext} from 'react'
+import AppContext from '../contexts/AppContext'
+
+const CompC = () => {
+  const { dispatchProvided } = useContext(AppContext)
+
+  return (
+    <div>
+      <button onClick={() => dispatchProvided('add_1')} >Add + 1</button>
+      <button onClick={() => dispatchProvided('multiple')}>Multiple * 3</button>
+      <button onClick={() => dispatchProvided('reset')}>Reset</button>
+    </div>
+  )
+}
+
+export default CompC
