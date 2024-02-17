9b248739a7ae8c1bfa22db277fc723877fb0e3b5
s-ko <song.1209@icloud.com>
2024-02-17 18:10:53 +0900

    feat: backendからtasksを取得、表示まで

--- frontend/src/components/DrfApiFetch.js
+++ frontend/src/components/DrfApiFetch.js
@@ -1,8 +1,25 @@
 import React, {useState, useEffect} from 'react'
+import axios from 'axios'
 
 const DrfApiFetch = () => {
+  const [tasks, setTasks] = useState([])
+  useEffect(() => {
+    axios.get('http://127.0.0.1:8000/api/tasks/', {
+      headers: {
+        'Authorization': 'Token ccd396af99a934a5290b68add5bb1dbeb812cb18'
+      }
+    })
+    .then(res => {setTasks(res.data)})
+  }, [])
+
   return (
-    <div>DrfApiFetch</div>
+    <div>
+      <ul>
+        {
+          tasks.map(task => <li key={task.id}> {task.id}: {task.title} </li>)
+        }
+      </ul>
+    </div>
   )
 }
 
